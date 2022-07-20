0e281e127701159c26fd2e30824b6be7e501eeef
Nick Pachulski <nick@pachulski.me>
2022-07-20 19:40:48 -0400

    wip

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
-	os.Stdout.Write([]byte("sum total: 2\n"))
+	operation := os.Args[2]
+	firstOperand, _ := strconv.Atoi(os.Args[1])
+	secondOperand, _ := strconv.Atoi(os.Args[3])
+	if operation == "+" {
+		fmt.Println(firstOperand + secondOperand)
+	}
+	if operation == "x" {
+		fmt.Println(firstOperand * secondOperand)
+	}
 }
--- main_test.go
+++ main_test.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"io/ioutil"
+	"fmt"
 	"os"
 	"strings"
 	"testing"
@@ -9,18 +9,58 @@ import (
 	"github.com/pjbgf/go-test/should"
 )
 
-func TestMain(t *testing.T) {
-	tmpfile, _ := ioutil.TempFile("", "calc-fake-stdout.*")
-	os.Stdout = tmpfile
-	defer os.Remove(tmpfile.Name())
-	os.Args = strings.Split("calc 1 + 1", " ")
+func TestAddition(t *testing.T) {
+	app := Run(main, "3 + 3")
+
+	should.New(t).BeEqual(
+		"6\n",
+		app.output(),
+		"Running 'calc 3 + 3' outputs '6'",
+	)
+
+	app.cleanup()
+}
+
+func TestMultiplication(t *testing.T) {
+	app := Run(main, "3 x 3")
+
+	should.New(t).BeEqual(
+		"9\n",
+		app.output(),
+		"Running 'calc 3 x 3' outputs '9'",
+	)
+
+	app.cleanup()
+}
+
+type commandLineApp struct {
+	realStandardOutput          *os.File
+	temporaryStandardOutputFile *os.File
+}
+
+func Run(main func(), commandLineArguments string) commandLineApp {
+	fakeAppName := "main"
+	fullRunCommand := fmt.Sprintf("%s %s", fakeAppName, commandLineArguments)
+	os.Args = strings.Split(fullRunCommand, " ")
+
+	realStandardOutput := os.Stdout
+	temporaryStandardOutputFile, _ := os.CreateTemp("", "")
+	os.Stdout = temporaryStandardOutputFile
 
 	main()
 
-	output, _ := ioutil.ReadFile(tmpfile.Name())
-	should.New(t).BeEqual(
-		"sum total: 2\n",
-		string(output),
-		"should sum 1+1 and return 2",
-	)
+	return commandLineApp{
+		realStandardOutput,
+		temporaryStandardOutputFile,
+	}
+}
+
+func (commandLineApp commandLineApp) output() string {
+	output, _ := os.ReadFile(commandLineApp.temporaryStandardOutputFile.Name())
+	return string(output)
+}
+
+func (commandLineApp commandLineApp) cleanup() {
+	os.Stdout = commandLineApp.realStandardOutput
+	os.Remove(commandLineApp.temporaryStandardOutputFile.Name())
 }
